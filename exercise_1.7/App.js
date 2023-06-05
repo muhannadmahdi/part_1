be5ea2086918e38a6c68deadd27c8c24e8013c01
muhannadmahdi <135234347+muhannadmahdi@users.noreply.github.com>
2023-06-05 20:32:21 +0300

    Add files via upload

---
+++ exercise_1.7/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0; // Avoid division by zero
+  const positivePercentage = (good / total) * 100 || 0; // Avoid division by zero
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+
+      <h2>Statistics</h2>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {total}</p>
+          <p>Average: {average}</p>
+          <p>Positive feedback: {positivePercentage}%</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
